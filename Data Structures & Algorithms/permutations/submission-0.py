5a2205f871b728d5cef5430d5d3b917b643f2c55
Elnathan Au <elnathanau1@gmail.com>
2026-07-16 10:19:06 -0700

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,33 @@
+import copy
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def backtrack(nums: List[int]) -> List[List[int]]:
+            if len(nums) == 0:
+                return []
+            if len(nums) == 1:
+                return [nums]
+            ret_list = []
+            for i in range(len(nums)):
+                temp = backtrack(nums[:i] + nums[i + 1:])
+                for possible in temp:
+                    ret_list.append([nums[i]] + possible)
+            return ret_list
+
+        return backtrack(nums)
+
+
+        return ret_list
+
+
+
+
+'''
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+'''
